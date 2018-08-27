c3aed851cf075f9ec4aec10b7cc484375fa88380
Jorge Victoria <jvictoria@alertlogic.com>
2018-08-27 18:20:03 -0500

    updated numpy test notebook

--- labs/3-computer-vision/notebooks/Numpy Test.py
+++ labs/3-computer-vision/notebooks/Numpy Test.py
@@ -1,29 +1,27 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[48]:
 
 
 import numpy as np
-from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity = "all"
 
 
 # ### Crear Vectores uni-, bi- o multi- dimensionales 
 
-# In[6]:
+# In[49]:
 
 
 np.array([1,2,3,4])
 
 
-# In[7]:
+# In[50]:
 
 
 np.array([[1,2],[3,4], [5,6]])
 
 
-# In[8]:
+# In[51]:
 
 
 np.array([[[1,2,3], [1,2,3]],[[1,2,3], [1,2,3]], [[1,2,3], [1,2,3]]])
@@ -31,19 +29,19 @@ np.array([[[1,2,3], [1,2,3]],[[1,2,3], [1,2,3]], [[1,2,3], [1,2,3]]])
 
 # ### Vector cero
 
-# In[9]:
+# In[52]:
 
 
 np.zeros(3)
 
 
-# In[10]:
+# In[53]:
 
 
 np.zeros((2,3))
 
 
-# In[11]:
+# In[54]:
 
 
 np.zeros((2,3,4))
@@ -51,7 +49,7 @@ np.zeros((2,3,4))
 
 # ### Dimensión de Arrays
 
-# In[12]:
+# In[55]:
 
 
 np.shape(np.array([1,2,3,4]))
@@ -59,7 +57,7 @@ np.shape(np.array([1,2,3,4]))
 
 # ### Acceso a Arrays
 
-# In[25]:
+# In[56]:
 
 
 x = np.array([[1,2],[3,4], [5,6]])
@@ -67,7 +65,7 @@ x[0]
 x[0,1]
 
 
-# In[22]:
+# In[57]:
 
 
 np.array([[1,2,3,4],[3,4,5,6], [2,3,3,4]])[0:3]
@@ -75,7 +73,7 @@ np.array([[1,2,3,4],[3,4,5,6], [2,3,3,4]])[0:3]
 
 # ### Operaciones
 
-# In[40]:
+# In[58]:
 
 
 x = np.array([[1,2,3,4],[3,4,5,6]])
@@ -86,7 +84,7 @@ x - x[1]
 np.abs(x - x[1])
 
 
-# In[45]:
+# In[59]:
 
 
 x = np.array([[[1,2,3],[3,4,5]], [[1,2,3], [3,4,5]]])
@@ -95,8 +93,14 @@ np.sum(x, axis=1)
 np.sum(x, axis=2)
 
 
-# In[47]:
+# In[60]:
 
 
 np.argmin(np.array([13,11,12]))
+
+
+# In[61]:
+
+
+get_ipython().system('ipython nbconvert --to=python "Numpy Test.ipynb"')
 
